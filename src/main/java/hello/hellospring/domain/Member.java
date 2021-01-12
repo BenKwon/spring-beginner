e02ad839b0217551efd4b81f4da275be56a50cc0
benkwon <kwon5891@gmail.com>
2021-01-12 12:29:03 +0900

    add jpa member repository

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,16 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
+
+//jpa가 관리하는 엔티티가 되는것이다
+@Entity
 public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) //DB가 알아서 생성해주는것 IDENTITY
     private Long id;
+
+  //  @Column(name = "username") DB의 컬럼명이 만약 USERNAME이면 NAME 과 USERNAME을 이러한 ANNOTATION으로 매핑
     private String name;
 
     public Long getId() {
